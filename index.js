65bc65a59559235cc88d9997055e9c6fe425d7b1
Nicholas Hanson-Holtry <nicholas.hanson.holtry@gmail.com>
2016-06-10 00:30:18 -0500

    very rough first draft

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/about-me', function(request, response) {
+  response.render('pages/about-me');
+});
+
+app.get('/cv-resume', function(request, response) {
+  response.render('pages/cv-resume');
+});
+
+app.get('/social-media', function(request, response) {
+  response.render('pages/social-media');
+});
+
 // run the app
 
 app.listen(app.get('port'), function() {
